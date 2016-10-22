8e7139191c9a3aeda23cf7b3f8379d0a5f00e86e
csb355 <201611113@sangmyung.kr>
2016-10-22 21:29:19 +0900

    DiceGame

---
+++ DiceGame/DiceGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class DiceGame {
+	public static void main(String[] args) {
+		String p1name;
+		Stirng p2name;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Input Player1 Name : ");
+		p1name = scanner.nextLine();
+		System.out.println("Input Player2 Name : ");
+		p2name = scanner.nextLine();
+	}
+	Player p1;
+	Player p2;
+	public void startGame () {
+		Player p1 = new Player(p1name);
+		Player p2 = new Player(p2name);
+		System.out.println(p1name + "'s turn");
+		p1.play();
+		System.out.println(p2name + "'s turn");
+		p2.play();
+		if (p1.getNum() > p2.getNum()) {
+           			System.out.println(p1.getName() + " won");
+        		}
+		else if (p1.getNum() == p2.getNum()) {
+		                System.out.println(" draw");
+		}
+		else
+		                System.out.println(p2.getName() + " won");
+		}
+}
