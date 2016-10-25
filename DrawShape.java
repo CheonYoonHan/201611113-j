b873c96091f1b25e01a84a730d4b26a31c3cec25
201611113 천윤한 <201611113@sangmyung.kr>
2016-10-26 04:24:31 +0900

    조건에 따라 도형그리기
    enum을 사용하여 조건에 맞는 도형을 그리게 해보았습니다.

---
+++ DrawShape.java
@@ -0,0 +1,31 @@
+import ch.aplu.turtle.*;
+public class DrawShape{
+	public static void main(String[] args){
+		new DrawShape().draw();
+	}
+	enum Shape{Triangle, Sqaure}
+	Shape sel=Shape.Triangle;
+	int size=100;
+	int sides;
+	double angle;
+
+	public DrawShape(){
+		switch (sel) {
+			case Triangle:
+				sides=3;
+				angle=120;
+				break;
+			case Sqaure:
+				sides=4;
+				angle=90;
+				break;
+		}
+	}
+	public void draw(){
+		Turtle t1=new Turtle();
+		for(int i=0; i<sides; i++) {
+			t1.forward(size);
+			t1.right(angle);
+		}
+	}
+}
