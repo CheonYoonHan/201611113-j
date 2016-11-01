7f2c8cd97721b7379de60d5089f7adcc2f9553a1
201611113 천윤한 <201611113@sangmyung.kr>
2016-11-01 21:22:56 +0900

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Grade{
+	public static void main(String[]args){
+		double marks;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input your marks : ");
+		marks = scanner.nextDouble();
+		if(90<=marks && marks <=100){
+			System.out.println("Your Grade is A");
+		}
+		else if(80<=marks && marks <90){
+			System.out.println("Your Grade is B");
+		}
+		else if(70<=marks && marks <80){
+			System.out.println("Your Grade is C");
+		}
+		else if(60<=marks && marks <70){
+			System.out.println("Your Grade is D");
+		}
+		else if(0<=marks && marks <60){
+			System.out.println("Your Grade is F");
+		} 
+		else {System.out.println("Error");}
+
+	}
+}
