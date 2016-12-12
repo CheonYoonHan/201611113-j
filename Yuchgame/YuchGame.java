046502430351eb7bf55666b24e5fb997d7c6c383
201611113 천윤한 <201611113@sangmyung.kr>
2016-12-12 15:49:41 +0900

    Update YuchGame.java

--- Yuchgame/YuchGame.java
+++ Yuchgame/YuchGame.java
@@ -1,12 +1,11 @@
 package com.sd.YuchGame;
 import java.util.Scanner;
 
-public class YuchGame1 extends DiceGame{
+public class YuchGame extends DiceGame{
 	public static void main(String[] args) {
 		String p1name;
 		String p2name;
 		Scanner scanner = new Scanner(System.in);
-
 		System.out.println("Input Player1 Name : ");
 		p1name = scanner.nextLine();
 		System.out.println("Input Player2 Name : ");
